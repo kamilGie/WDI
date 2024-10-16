2b913062884153070bb0468330a9c0e9f8d1bf1b
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-10-16 23:56:03 +0200

    test 01

--- Zestaw_1:_Proste_programy_z_pętlami/testy/t01.py
+++ Zestaw_1:_Proste_programy_z_pętlami/testy/t01.py
@@ -9,30 +9,60 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.abspath(os.path.join(current_dir, os.pardir)))
 Zadanie_1 = importlib.import_module("01").Zadanie_1
 
-# testy pisze sie kopiujac jedna z tych funkcji i zmieniajac nazwe. trzeba zostawic przedrostek test_<tutaj dowolnosci> 
+# testy pisze sie kopiujac jedna z tych funkcji i zmieniajac nazwe. trzeba zostawic przedrostek test_<tutaj dowolnosci>
 # jesli funkcja przyjmuje wartosci trzeba dodac do wywolan aby testy dzialaly
 
 
 class Test_1(unittest.TestCase):
-    def test_BrakTestow(self):  # po napisaniu testu usunac ta funkcje
-        self.assertEqual("brak napisanych testow do tego zadania", "")
-
-    def test_wypisujacy(self):
-        # te komendy przejmuja wyniki printa
+    def test_nRowne1(self):
         captured_output = io.StringIO()
         sys.stdout = captured_output
 
-        Zadanie_1()
+        Zadanie_1(1)
 
-        # przestajemy przejmowac wyniki printa i zwracamy jakie zmienne wypisalo po odpaleniu funkcji
         sys.stdout = sys.__stdout__
         wynik = captured_output.getvalue().strip()
 
         prawdziwyWynik = ""
         self.assertEqual(wynik, prawdziwyWynik)
 
+    def test_n_rowne_2(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        Zadanie_1(2)
+
+        sys.stdout = sys.__stdout__
+        wynik = captured_output.getvalue().strip()
+
+        prawdziwyWynik = "1 1 1"
+        self.assertEqual(wynik, prawdziwyWynik)
+
+    def test_n_rowne_3(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        Zadanie_1(3)
+
+        sys.stdout = sys.__stdout__
+        wynik = captured_output.getvalue().strip()
+
+        prawdziwyWynik = "1 1 1\n1 1 2\n1 2 1\n1 2 2\n2 1 1\n2 1 2\n2 2 1\n2 2 2"
+        self.assertEqual(wynik, prawdziwyWynik)
+
+    def test_n_rowne_4(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+
+        Zadanie_1(4)
+
+        sys.stdout = sys.__stdout__
+        wynik = captured_output.getvalue().strip()
+        prawdziwyWynik = "1 1 1\n1 1 2\n1 1 3\n1 2 1\n1 2 2\n1 2 3\n1 3 1\n1 3 2\n1 3 3\n2 1 1\n2 1 2\n2 1 3\n2 2 1\n2 2 2\n2 2 3\n2 3 1\n2 3 2\n2 3 3\n3 1 1\n3 1 2\n3 1 3\n3 2 1\n3 2 2\n3 2 3\n3 3 1\n3 3 2\n3 3 3"
+        self.assertEqual(wynik, prawdziwyWynik)
+
     def test_zwracajacy(self):
-        wynik = Zadanie_1()
+        wynik = Zadanie_1(2)
         prawdziwyWynik = None
         self.assertEqual(wynik, prawdziwyWynik)
 
