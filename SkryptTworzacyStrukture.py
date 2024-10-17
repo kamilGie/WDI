b660fb2758bd550e3a462b4117870a67162dbd5f
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-10-17 17:09:15 +0200

    nowa struktura folderowa

--- SkryptTworzacyStrukture.py
+++ SkryptTworzacyStrukture.py
@@ -2,33 +2,50 @@ import pdfplumber
 import os
 
 
-# test
-def GenerujTest(numer_zadania):
+def GenerujWstep(zadanie, nrZadania):
+    obramowka = "# ====================================================================================================>\n"
+    zadanie_tresc = zadanie[zadanie.find(".") + 2 :].replace("\n", "\n# ")
+
+    # czasami to sie przedluzalo nwm czemu ale to naprawia xd
+    if zadanie_tresc[-2] == "#":
+        zadanie_tresc = zadanie_tresc[:-3]
+
+    return (
+        f"{obramowka}" f"# Zadanie {nrZadania}\n" f"# {zadanie_tresc}\n" f"{obramowka}"
+    )
+
+
+def GenerujZawartosci(nrZadania):
+    return f"""\n\ndef Zadanie_{nrZadania}(): ...\n\n
+if __name__ == "__main__":
+    from test{nrZadania} import odpalTesty
+
+    Zadanie_{nrZadania}()
+    # odpalTesty()
+"""
+
+
+def GenerujTest(nrZadania):
     return f"""import unittest
 import sys
 import io
-import os
-import importlib
 
-# Dodanie dynamicznej ścieżki do katalogu nadrzędnego
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.abspath(os.path.join(current_dir, os.pardir)))
-Zadanie_{numer_zadania} = importlib.import_module("{numer_zadania:02}").Zadanie_{numer_zadania}
+from exercise{nrZadania} import Zadanie_{nrZadania}
+
+TESTY = False  # po napisaniu testow zmienic na true
 
 # testy pisze sie kopiujac jedna z tych funkcji i zmieniajac nazwe. trzeba zostawic przedrostek test_<tutaj dowolnosci> 
 # jesli funkcja przyjmuje wartosci trzeba dodac do wywolan aby testy dzialaly
 
 
-class Test_{numer_zadania}(unittest.TestCase):
-    def test_BrakTestow(self):  # po napisaniu testu usunac ta funkcje
-        self.assertEqual("brak napisanych testow do tego zadania", "")
+class Test{nrZadania}(unittest.TestCase):
 
-    def test_wypisujacy(self):
+    def test_wypisywania(self):
         # te komendy przejmuja wyniki printa
         captured_output = io.StringIO()
         sys.stdout = captured_output
 
-        Zadanie_{numer_zadania}()
+        Zadanie_{nrZadania}()
 
         # przestajemy przejmowac wyniki printa i zwracamy jakie zmienne wypisalo po odpaleniu funkcji
         sys.stdout = sys.__stdout__
@@ -37,15 +54,44 @@ class Test_{numer_zadania}(unittest.TestCase):
         prawdziwyWynik = ""
         self.assertEqual(wynik, prawdziwyWynik)
 
-    def test_zwracajacy(self):
-        wynik = Zadanie_{numer_zadania}()
+    def test_zwracania(self):
+        wynik = Zadanie_{nrZadania}()
         prawdziwyWynik = None
         self.assertEqual(wynik, prawdziwyWynik)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def odpalTesty():
+    assert TESTY, "Testy do tego zadania nie zostaly jeszcze napisane"
+    suite = unittest.TestLoader().loadTestsFromTestCase(Test{nrZadania})
+    unittest.TextTestRunner(verbosity=2).run(suite)
 """
+
+
+def StworzPlikiPython(zawartosc, pelnaSciezka):
+    sciezka_pliku = os.path.join(pelnaSciezka)
+    try:
+        with open(sciezka_pliku, "w") as plik:
+            plik.write(zawartosc)
+    except IOError as e:
+        print(f"Nie udało się zapisać pliku {sciezka_pliku}: {e}")
+
+
+def StworzFolderZadania(FolderZadania, nrZadania, Zadanie):
+    os.makedirs(FolderZadania)
+
+    wstep = GenerujWstep(Zadanie, nrZadania)
+    StworzPlikiPython(
+        wstep + GenerujZawartosci(nrZadania),
+        f"{FolderZadania}/exercise{nrZadania}.py",
+    )
+    StworzPlikiPython(
+        wstep + "\n\n\n",
+        f"{FolderZadania}/solution{nrZadania}.py",
+    )
+    StworzPlikiPython(
+        GenerujTest(nrZadania),
+        f"{FolderZadania}/test{nrZadania}.py",
+    )
 
 
 def PodzielTextNaZestawy(text):
@@ -63,40 +109,6 @@ def PodzielTextNaZestawy(text):
     return Zestawy
 
 
-def StworzPlikTestowy(nrZadania, DirTestowe):
-    sciezka_pliku = os.path.join(DirTestowe, f"t{nrZadania:02}.py")
-    try:
-        with open(sciezka_pliku, "w") as plik:
-            plik.write(GenerujTest(nrZadania))
-    except IOError as e:
-        print(f"Nie udało się zapisać pliku testowego {sciezka_pliku}: {e}")
-
-
-def StworzPlikPython(zadanie, nrZadania, nazwaZestawu):
-    obramowka = "# ====================================================================================================>\n"
-    zadanie_tresc = zadanie[zadanie.find(".") + 2 :].replace("\n", "\n# ")
-
-    # czasami to sie przedluzalo nwm czemu ale to naprawia xd
-    if zadanie_tresc[-2] == "#":
-        zadanie_tresc = zadanie_tresc[:-3]
-
-    zawartoscPliku = (
-        f"{obramowka}"
-        f"# Zadanie {nrZadania}\n"
-        f"# {zadanie_tresc}\n"
-        f"{obramowka}\n\n"
-        f"def Zadanie_{nrZadania}(): ...\n\n\n"
-        f"Zadanie_{nrZadania}()\n"
-    )
-
-    sciezka_pliku = os.path.join(nazwaZestawu, f"{nrZadania:02}.py")
-    try:
-        with open(sciezka_pliku, "w") as plik:
-            plik.write(zawartoscPliku)
-    except IOError as e:
-        print(f"Nie udało się zapisać pliku {sciezka_pliku}: {e}")
-
-
 def Skrypt():
     with pdfplumber.open("ZbiorZadan.pdf") as pdf:
         # lacze cale strony zbioru zadan po za ostatnimy 2 literami bo to numery strony
@@ -109,7 +121,6 @@ def Skrypt():
             PoczatekNastZad = zestaw.find("Zadanie")
             nazwaZestawu = zestaw[:PoczatekNastZad].replace(" ", "_").replace("\n", "")
             os.makedirs(nazwaZestawu)
-            os.makedirs(nazwaZestawu + "/testy")
 
             while PoczatekNastZad != -1:
                 zestaw = zestaw[PoczatekNastZad:]
@@ -117,8 +128,11 @@ def Skrypt():
                 if PoczatekNastZad == -1:  # czasami pdf reader usuwa spacje pomiedzy
                     PoczatekNastZad = zestaw.find(f"Zadanie{nrZadania+1}.")
 
-                StworzPlikPython(zestaw[:PoczatekNastZad], nrZadania, nazwaZestawu)
-                StworzPlikTestowy(nrZadania, nazwaZestawu + "/testy")
+                StworzFolderZadania(
+                    nazwaZestawu + f"/{nrZadania:02}",
+                    nrZadania,
+                    zestaw[:PoczatekNastZad],
+                )
 
                 nrZadania += 1
 
---
+++ Zestaw_1:_Proste_programy_z_pętlami/01/solution1.py
@@ -0,0 +1,5 @@
+# ====================================================================================================>
+# Zadanie 1
+# Proszę napisać program poszukujący trójkątów Pitagorejskich w których długość przekątnej
+# jest mniejsza od liczby N wprowadzonej z klawiatury.
+# ====================================================================================================>
