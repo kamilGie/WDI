703600ee2e0efc4d2aaa0f6c942ed85a82ff8247
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-10-18 18:26:01 +0200

    patryk dodal tu jakies heady i tego nie zobaczylem naprawiam to

--- Zestaw_1:_Proste_programy_z_pętlami/02/test2.py
+++ Zestaw_1:_Proste_programy_z_pętlami/02/test2.py
@@ -1,21 +1,12 @@
+from contextlib import redirect_stdout
 import unittest
-<<<<<<< HEAD
-from contextlib import redirect_stdout
-=======
->>>>>>> upstream/solutions
 import io
-from contextlib import redirect_stdout
 
 from exercise2 import Zadanie_2
 
 TESTY = False  # po napisaniu testow zmienic na true
 
-<<<<<<< HEAD
-# testy pisze sie kopiujac jedna z tych funkcji i zmieniajac nazwe. trzeba zostawic przedrostek test_<tutaj dowolnosci>
-=======
-
 # testy pisze sie kopiujac jedna z tych funkcji i zmieniajac nazwe. trzeba zostawic przedrostek test_<tutaj dowolnosci> 
->>>>>>> upstream/solutions
 # jesli funkcja przyjmuje wartosci trzeba dodac do wywolan aby testy dzialaly
 class Test2(unittest.TestCase):
 
@@ -25,13 +16,8 @@ class Test2(unittest.TestCase):
             Zadanie_2()
         wynik = f.getvalue().strip()
 
-<<<<<<< HEAD
-        prawdziwyWynik = ""
-        self.assertEqual(wynik, prawdziwyWynik)
-=======
         oczekiwany_wynik = ""
         self.assertEqual(wynik, oczekiwany_wynik)
->>>>>>> upstream/solutions
 
     def test_zwracania(self):
         wynik = Zadanie_2()
