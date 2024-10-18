54cb7ab19ea0df40d5ccd36fc670f5a819776f33
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-10-19 00:05:38 +0200

    2

--- Zestaw_1:_Proste_programy_z_pętlami/02/test2.py
+++ Zestaw_1:_Proste_programy_z_pętlami/02/test2.py
@@ -4,9 +4,10 @@ import io
 
 from exercise2 import Zadanie_2
 
-TESTY = False  # po napisaniu testow zmienic na true
+TESTY = True  # po napisaniu testow zmienic na true
 
-# testy pisze sie kopiujac jedna z tych funkcji i zmieniajac nazwe. trzeba zostawic przedrostek test_<tutaj dowolnosci> 
+
+# testy pisze sie kopiujac jedna z tych funkcji i zmieniajac nazwe. trzeba zostawic przedrostek test_<tutaj dowolnosci>
 # jesli funkcja przyjmuje wartosci trzeba dodac do wywolan aby testy dzialaly
 class Test2(unittest.TestCase):
 
@@ -16,13 +17,7 @@ class Test2(unittest.TestCase):
             Zadanie_2()
         wynik = f.getvalue().strip()
 
-        oczekiwany_wynik = ""
-        self.assertEqual(wynik, oczekiwany_wynik)
-
-    def test_zwracania(self):
-        wynik = Zadanie_2()
-
-        oczekiwany_wynik = None
+        oczekiwany_wynik = "1\n1\n2\n3\n5\n8\n13\n21\n34\n55\n89\n144\n233\n377\n610\n987\n1597\n2584\n4181\n6765\n10946\n17711\n28657\n46368\n75025\n121393\n196418\n317811\n514229\n832040"
         self.assertEqual(wynik, oczekiwany_wynik)
 
 
