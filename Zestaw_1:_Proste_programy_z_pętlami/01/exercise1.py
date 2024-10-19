ccbeb32cb190d05426a08bd0df825d846a3255ca
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-10-19 19:36:47 +0200

    zadanie 1

--- Zestaw_1:_Proste_programy_z_pętlami/01/exercise1.py
+++ Zestaw_1:_Proste_programy_z_pętlami/01/exercise1.py
@@ -3,14 +3,19 @@
 # Proszę napisać program poszukujący trójkątów Pitagorejskich w których długość przekątnej
 # jest mniejsza od liczby N wprowadzonej z klawiatury.
 # ====================================================================================================>
-# print(katA,katB,katC)
+# print(a, b, int(c))
 
 
-def Zadanie_1(n): ...
+def Zadanie_1(n):
+    for a in range(1, n):
+        for b in range(a, n):
+            c = (a * a + b * b) ** 0.5
+            if c.is_integer() and c < n:
+                print(a, b, int(c))
 
 
 if __name__ == "__main__":
     from test1 import odpalTesty
 
-    Zadanie_1(input())
-    # odpalTesty()
+    # Zadanie_1(int(input()))
+    odpalTesty()
