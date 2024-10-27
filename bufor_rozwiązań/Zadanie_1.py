77c28b8ac4121e33bc67244979fea0f8a0c23ecd
PatrykPietryka <164105624+PatrykPietryka@users.noreply.github.com>
2024-10-27 20:47:57 +0100

    Create Zadanie_1.py

---
+++ bufor_rozwiązań/Zadanie_1.py
@@ -0,0 +1,6 @@
+def Zadanie_1(n):
+    for a in range(1, n):
+        for b in range(a, n):
+            c = (a * a + b * b) ** 0.5
+            if c.is_integer() and c < n:
+                print(a, b, int(c))
