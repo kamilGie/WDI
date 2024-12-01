561a1906591aac1013af5e06081d0532201bc632
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-12-01 18:30:58 +0100

    kolosy 2 2020

--- Kolokwia/Kolokwium_2/2020_4/szablon168.py
+++ Kolokwia/Kolokwium_2/2020_4/szablon168.py
@@ -13,36 +13,12 @@
 # ====================================================================================================>
 
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    if num == 2 or num == 3:
-        return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
-
-    for i in range(6, int(num**0.5) + 3, 6):
-        if num % (i + 1) == 0 or num % (i - 1) == 0:
-            return False
-
-    return True
+def divide(N): ...
 
 
-def rek(num, nums, i, parts):
-    # print(num, nums, i, parts)
+if __name__ == "__main__":
+    from testy168 import odpal_testy
 
-    if is_prime(num) and is_prime(parts):
-        return True
+    # divide(int(input('Podaj N: ')))
 
-    if i > num:
-        return False
-
-    if is_prime(num % i):
-        if rek(num // i, [num % i] + nums, 10, parts + 1):
-            return True
-
-    return rek(num, [*nums], i * 10, parts)
-
-
-def divide(N):
-    return rek(N, [], 10, 1)
+    odpal_testy()
