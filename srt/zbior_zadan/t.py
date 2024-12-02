7771b52d9661c152fdc785fb5ff51489f3f15eee
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-12-02 23:34:35 +0100

    czarne proba 3

--- srt/zbior_zadan/t.py
+++ srt/zbior_zadan/t.py
@@ -7,36 +7,28 @@ def apply_custom_color_mapping(
     output_folder,
     prefix="black_",
     white_replacement=(38, 44, 54, 255),
-    black_replacement=(101, 108, 118, 51),
+    black_replacement=(101, 108, 118, 255),
 ):
-    # Tworzenie folderu wyjściowego, jeśli nie istnieje
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     for filename in os.listdir(input_folder):
         if filename.endswith((".png", ".jpg", ".jpeg")):
-            # Ścieżki wejściowe i wyjściowe
             input_path = os.path.join(input_folder, filename)
             output_path = os.path.join(output_folder, f"{prefix}{filename}")
 
-            # Otwórz obraz
             with Image.open(input_path).convert("RGBA") as img:
-                # Utwórz tło w zadanym kolorze
 
-                # Przeprowadź zamianę kolorów
                 width, height = img.size
                 pixels = img.load()
                 for x in range(width):
                     for y in range(height):
                         r, g, b, a = pixels[x, y]
 
-                        # Oblicz jasność jako średnią RGB
                         brightness = (r + g + b) / 3
 
-                        # Oblicz współczynnik jasności
                         t = brightness / 255
 
-                        # Interpolacja między kolorami
                         new_r = int(
                             black_replacement[0] * (1 - t) + white_replacement[0] * t
                         )
@@ -50,16 +42,14 @@ def apply_custom_color_mapping(
                             black_replacement[3] * (1 - t) + white_replacement[3] * t
                         )
 
-                        # Ustaw nowy kolor piksela
                         pixels[x, y] = (new_r, new_g, new_b, new_a)
 
-                # Zapisz obraz wynikowy
                 img.save(output_path)
 
 
 # Użycie
-input_folder = "."  # Folder z obrazami wejściowymi
-output_folder = "."  # Folder na zapisane obrazy
+input_folder = "."
+output_folder = "."
 apply_custom_color_mapping(
     input_folder,
     output_folder,
