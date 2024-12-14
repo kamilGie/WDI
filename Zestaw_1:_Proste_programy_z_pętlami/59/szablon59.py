f0c03f98a65a1a5a5e9d3a2f250a7d2e74a86cb3
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-12-14 11:55:49 +0100

    Update szablon59.py

--- Zestaw_1:_Proste_programy_z_pętlami/59/szablon59.py
+++ Zestaw_1:_Proste_programy_z_pętlami/59/szablon59.py
@@ -3,7 +3,7 @@
 # Tylko 7 liczb pierwszych spełnia warunek z poprzedniego zadania. Proszę napisać program
 # znajdujący wszystkie te liczby.
 # ====================================================================================================>
-# zwrocic od spacji te liczby 2 3 5 7 28116440335967 449177399146038697307 35452590104031691935943
+# wypisać od spacji te liczby 2 3 5 7 28116440335967 449177399146038697307 35452590104031691935943
 
 
 def narcystyczne_pierwsze(): ...
